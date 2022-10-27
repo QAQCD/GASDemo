585a4372ea215aae557dbc8646ca5249fd86aa7b
QAQCD <90947622+QAQCD@users.noreply.github.com>
2022-10-27 11:54:15 +0800

    武器管理器

---
+++ Source/GASDemo/Public/GWeaponsManager.h
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+
+#include "GWeaponActor.h"
+#include "GWeaponsManager.generated.h"
+
+
+//定义枚举值双手武器还是单手武器
+UENUM(BlueprintType)
+enum class ESingleOrDouble : uint8
+{
+	Left,
+	Right,
+	SingleOrDouble
+};
+
+
+class USceneComponent;
+
+//UCLASS 定义中添加“Blueprintable”蓝图继承
+UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class GASDEMO_API UGWeaponsManager : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGWeaponsManager();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	
+public:
+	//自定义
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapons")
+	ESingleOrDouble SingleOrDouble = ESingleOrDouble::Right;
+
+	//左手
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapons", meta=(EditCondition = "SingleOrDouble == ESingleOrDouble::Left || SingleOrDouble == ESingleOrDouble::SingleOrDouble", EditConditionHides))
+	TSubclassOf<AActor> L_WeaponClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapons", meta = (EditCondition = "SingleOrDouble == ESingleOrDouble::Left || SingleOrDouble == ESingleOrDouble::SingleOrDouble", EditConditionHides))
+	FName L_SocketName;
+
+
+	//右手
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapons", meta = (EditCondition = "SingleOrDouble == ESingleOrDouble::Right || SingleOrDouble == ESingleOrDouble::SingleOrDouble", EditConditionHides))
+	TSubclassOf<AGWeaponActor> R_WeaponClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapons", meta = (EditCondition = "SingleOrDouble == ESingleOrDouble::Right || SingleOrDouble == ESingleOrDouble::SingleOrDouble", EditConditionHides))
+	FName R_SocketName;
+
+};
